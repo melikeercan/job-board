cdd915fa7a9d519db1f0f4b60c4990455ed77f7d
Melike Ercan <melikeercan@Melikes-MacBook-Pro.local>
2023-06-09 11:29:00 +0200

    data types

--- server/resolvers.js
+++ server/resolvers.js
@@ -1,5 +1,20 @@
 export const resolvers = {
     Query: {
-        greeting: () => 'Hello GraphQL world!',
+        jobs: () => {
+
+            return [
+                {
+                    id: '1',
+                    title: 'Software Engineer',
+                    description: 'Awesome job',
+                },
+                {
+                    id: '2',
+                    title: 'Senior Software Engineer',
+                    description: 'More Awesome job',
+                },
+
+            ]
+        }
     }
 };
